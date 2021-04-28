2bd4b165d14ebbb42a06c0d4878fdf341920fa1e
Vicky Loebel <vickyloebel@gmail.com>
2021-04-27 21:47:14 -0700

    created portfolio.json to load work samples

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,33 +1,14 @@
 import React from "react";
-const IMGPATH = "../../assets/images/"
-const projectList = [
-  {
-    "id": "1",
-    "name": `back2work`,
-    "skills": `MVC architecture (full stack), mySql/Sequelize, handlebars.js`,
-    "srcImg": 'back2work-login.jpg',
-    "srcAlt": 'back2work logo',
-    "appLink": "https://vllback2work.herokuapp.com"
-  },
-  {
-    "id": "2",
-    "name": `day-scheduler`,
-    "skills": `javascript`,
-    "srcImg": 'day-schedule.jpg',
-    "srcAlt": 'back2work logo',
-    "appLink": "more http stuff"
-  }
-]
-//     "srcImg": `require('../../assets/images/day-schedule.jpg/')`,
-//     <img src={require('../../assets/images/back2work-login.jpg').default} alt={item.alt} />
+import myProjectList from './portfolio.json';
 
+// const IMGPATH = "../../assets/images/"
 
-// (props) 
+// (props) ?
 const Portfolio = () => {
   return (
     // return a section containing item divs
     <section className="portfolio-section">
-      {projectList.map(item => (
+      {myProjectList.map(item => (
         <div className="portfolio-item" key={item.id}>
           <h2>{item.name}</h2>
           <h3>{item.skills}</h3>

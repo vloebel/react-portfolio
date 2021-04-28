99fc63919a9658b4c9db8a51583e7058fa2cc516
Vicky Loebel <vickyloebel@gmail.com>
2021-04-27 20:35:54 -0700

    got pictures to load! yay!

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,18 +1,43 @@
 import React from "react";
+const projectList = [
+  {
+    "id": "1",
+    "name": `back2work`,
+    "skills": `MVC architecture (full stack), mySql/Sequelize, handlebars.js`,
+    "srcImg": 'back2work-login.jpg',
+    "srcAlt": 'back2work logo',
+    "appLink": "https://vllback2work.herokuapp.com"
+  },
+  {
+    "id": "2",
+    "name": `day-scheduler`,
+    "skills": `javascript`,
+    "srcImg": 'day-schedule.jpg',
+    "srcAlt": 'back2work logo',
+    "appLink": "more http stuff"
+  }
+]
+//     "srcImg": `require('../../assets/images/day-schedule.jpg/')`,
+//     <img src={require('../../assets/images/back2work-login.jpg').default} alt={item.alt} />
 
 
 // (props) 
-  const Portfolio = () => {
-    // const { currentCategory } = props;
+const Portfolio = () => {
   return (
-    <section>
-      {/* <h1 data-testid="h1tag">{currentCategory.name}</h1> */}
-      <h1>Web Development Portfolio</h1>
-      {/* <p>{currentCategory.description}</p> */}
-    <p> Donec a volutpat quam.</p>
-      {/* <ProjectList category={currentCategory.name} /> */}
+    // return a section containing item divs
+    <section className="portfolio-section">
+      {projectList.map(item => (
+        <div className="portfolio-item" key={item.id}>
+          <h2>{item.name}</h2>
+          <h3>{item.skills}</h3>
+          <a href={item.appLink}>
+            <img src={require(`../../assets/images/${item.srcImg}`).default} alt={item.alt} />
+          </a>
+        </div>
+      )
+      )}
     </section>
-  );
+  )
 }
 
 export default Portfolio;

3f9f122624a83cd1cdaa4f2d98163c9bd4b538d3
Vicky Loebel <vickyloebel@gmail.com>
2021-04-29 17:29:24 -0700

    first round of styling complete

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,12 +2,11 @@ import React from "react";
 
 const About = () => (
   <div>
-    <h1>About Page</h1>
-    <img src={require(`../../assets/images/vickyphoto.jpg`).default} alt="vicky loebel headshot" />
 
-    <p>
+    <img className="bio-pic" src={require(`../../assets/images/vickyphoto.jpg`).default} alt="vicky loebel headshot" />
+    
     Vicky Loebel began her technical career as a systems programmer on the flight simulators at the NASA Ames research facility in Silicon Valley, followed by several years' experience as a contract coder and software technical writer. Her education includes a BA in Lingustics from the University of Wisconsin and a Masters of Computer Science from San Jose State. She is currently enrolled in the University of Arizona's Full-Stack Web Developemnt Bootcamp with the goal of seeking freelance work in website design and development.
-    </p>
+    
   </div>
 );
 
--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => (
     <article id="contact-me" class="flexbox-container">
-    <h1>Contact Me</h1>
+    <h2>Contact Me</h2>
     <div>
       <ul class="contact-info">
-        <li><a href="tel:1-520-760-3300">520 760-3300</a></li>
+        {/* <li><a href="tel:1-520-760-3300">520 760-3300</a></li> */}
         <li><a href="https://vickyloebel.com/wp/">Vicky Loebel.com</a> </li>
         <li><a href="https://github.com/vloebel">GitHub</a> </li>
         <li><a href="https://www.linkedin.com/in/vicky-loebel-4832a6201/">LinkedIn</a></li>

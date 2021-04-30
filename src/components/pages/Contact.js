461cbce9cb8750adad9c623f780203fa285ef1cf
Vicky Loebel <vickyloebel@gmail.com>
2021-04-30 12:28:46 -0700

    contact field done with submit

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,68 +4,65 @@ import { validateEmail } from '../../utils/helpers';
 
 
 const Contact = () => {
-  
-const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-const [errorMessage, setErrorMessage] = useState('');
-const { name, email, message } = formState;
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  if (!errorMessage) {
-    console.log('Submit Form', formState);
-  }
-};
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
 
-const handleChange = (e) => {
-  if (e.target.name === 'email') {
-    const isValid = validateEmail(e.target.value);
-    if (!isValid) {
-      setErrorMessage('Please enter a valid email.');
-    } else {
-      setErrorMessage('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
     }
-  } else {
-    if (!e.target.value.length) {
-      setErrorMessage(`${e.target.name} is required.`);
-    } else {
-      setErrorMessage('');
-    }
-  }
-  if (!errorMessage) {
-    setFormState({ ...formState, [e.target.name]: e.target.value });
-    console.log('Handle Form', formState);
-  }
-};
+  };
 
-return (
-  <section >
-    <h1>Contact me</h1>
-    <div className="contact-container">
-    <form id="contact-form" onSubmit={handleSubmit}>
-      <div className="contact-field">
-        <label className="input-label" htmlFor="name">Name:</label>
-        <input className="input-field" type="text" name="name" defaultValue={name} onBlur={handleChange} />
-      </div>
-      <div className="contact-field">
-        <label className="input-label"htmlFor="email">Email address:</label>
-        <input className="input-field" type="email" name="email" defaultValue={email} onBlur={handleChange} />
-      </div>
-      <div  className="contact-field">
-        <label className="input-label" htmlFor="message">Message:</label>
-        <textarea className="input-field"  name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-      </div>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-        )}
-        
-      <button  type="submit">Submit</button>
-      </form>
-      </div>
-  </section>
-);
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('please enter a valid email');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      // console.log('Handle Form', formState);
+    }
+  };
+
+  return (
+    <section >
+      <h1>Contact me</h1>
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <div className="contact-field">
+            <label htmlFor="name">Name:</label>
+            <input className="input-field" type="text" name="name" defaultValue={name} onMouseOut={handleChange} />
+          </div>
+          <div className="contact-field">
+            <label htmlFor="email">Email:</label>
+            <input className="input-field" type="email" name="email" defaultValue={email} onMouseOut={handleChange} />
+          </div>
+          <div className="contact-field">
+            <label htmlFor="message">Message:</label>
+            <textarea className="input-field" name="message" rows="5" defaultValue={message} onMouseOut={handleChange} />
+          </div>
+          <div>
+              {errorMessage && (
+                <p className="error-text">{errorMessage}</p>
+              )}
+          </div>
+          <button className="btn-submit" type="submit">Submit</button>
+        </form>
+    </section>
+  );
 }
 
 

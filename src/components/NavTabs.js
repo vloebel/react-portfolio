1fcdec9721a89bdd5cecc0d34f785c202ad9aeb5
Vicky Loebel <vickyloebel@gmail.com>
2021-05-01 13:37:03 -0700

    got nav-link-active working

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -17,7 +17,7 @@ function NavTabs(props) {
               // page w handlePageChange props.
               onClick={() => props.handlePageChange(tab)}
               className={
-                props.currentPage === tab ? 'nav-link active' : 'nav-link'
+                props.currentPage === tab ? 'nav-link-active' : 'nav-link'
               }
             >
               {tab}
